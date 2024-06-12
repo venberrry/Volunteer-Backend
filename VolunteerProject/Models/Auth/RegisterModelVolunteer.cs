06b16168fb5b0b47ad006f60aa3cc1458cf1f763
sergey2k161 <joysergey4@gmail.com>
2024-06-12 17:36:00 +0300

    Auth Models

---
+++ VolunteerProject/Models/Auth/RegisterModelVolunteer.cs
@@ -0,0 +1,15 @@
+﻿namespace VolunteerProject.Models.Auth;
+
+using System.ComponentModel.DataAnnotations;
+
+public class RegisterModelVolunteer
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+}
