372b99d7804f37c59cc0309f91cedec20e9b3c78
sergey2k161 <joysergey4@gmail.com>
2024-06-14 00:15:00 +0300

    files

---
+++ VolunteerProject/Models/BaseModels/OrganizationMembership.cs
@@ -0,0 +1,19 @@
+﻿namespace VolunteerProject.Models;
+
+using System.ComponentModel.DataAnnotations;
+public class OrganizationMembership
+{
+    [Key]
+    public int Id { get; set; }
+    
+    [Required]
+    public int OranizationId { get; set; }
+    public Organization Organization { get; set; }
+    
+    [Required]
+    public int UserId { get; set; }
+    public User User { get; set; }
+    
+    [Required]
+    public DateTime JoinDate { get; set; }
+}
