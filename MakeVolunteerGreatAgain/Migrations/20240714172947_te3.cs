574e952d0fb3a32e27d334b02222020148cefb5e
sergey2k161 <joysergey4@gmail.com>
2024-07-15 01:45:54 +0300

    Мб так?

---
+++ MakeVolunteerGreatAgain/Migrations/20240714172947_te3.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MakeVolunteerGreatAgain.Migrations
+{
+    /// <inheritdoc />
+    public partial class te3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Organizations_OrganizationId",
+                table: "Events");
+
+            migrationBuilder.RenameColumn(
+                name: "OrganizationId",
+                table: "Events",
+                newName: "OrganizationCommunUserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Events_OrganizationId",
+                table: "Events",
+                newName: "IX_Events_OrganizationCommunUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Organizations_OrganizationCommunUserId",
+                table: "Events",
+                column: "OrganizationCommunUserId",
+                principalTable: "Organizations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Organizations_OrganizationCommunUserId",
+                table: "Events");
+
+            migrationBuilder.RenameColumn(
+                name: "OrganizationCommunUserId",
+                table: "Events",
+                newName: "OrganizationId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Events_OrganizationCommunUserId",
+                table: "Events",
+                newName: "IX_Events_OrganizationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Organizations_OrganizationId",
+                table: "Events",
+                column: "OrganizationId",
+                principalTable: "Organizations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
